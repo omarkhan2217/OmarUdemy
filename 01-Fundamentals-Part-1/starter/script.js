7391d61ab7ed8f72944fa4bc008ec96a8428146f
Omar Khan <ok.66677788@gmail.com>
2021-10-13 18:24:02 +0530

    innitial

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,46 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("jonas");
+// console.log(23);
+
+// let firstName = "new";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// //Variable Name Conventions
+// let jonas_matilda = "jm";
+// let $function = 27;
+
+// let person = "jonas";
+// let PI = 3.1415;
+
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Teacher";
+
+// let job1 = "programmer";
+// let job2 = "teacher";
+
+// console.log(myFirstJob);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// // console.log(typeof true)
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof "jonas");
+
+// javascriptIsFun = "YES!";
+// console.log(javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
