54f29cbccebf9ae368ad7afa0db75312876f4937
Omar Khan <ok.66677788@gmail.com>
2021-10-14 23:42:12 +0530

    added

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,37 @@
+'use strict'
+
+
+
+// LESSON 32 = ACTIVATING STRICT MODE:-
+
+// let hasDriversLicense = false
+// const passTest = true
+
+// if (passTest) hasDriversLicense = true
+// if (hasDriversLicense) console.log(`HAUEHUAWEH`)
+
+// // const interface = 'Audio'
+// // const private = 21
+
+
+
+// LESSON 33 = FUNCTIONS:-
+
+// function logger() {
+//     console.log(`my name is aaaa`)
+// }
+
+// logger()
+
+
+// function foodProcessor(apples, oranges) {
+//     console.log(apples, oranges)
+//     const juice = `Juice with ${apples} fresh apples and ${oranges} fresh oranges`
+//     return juice
+// }
+
+// const appleJuice = foodProcessor(5, 0)
+// console.log(appleJuice)
+
+// const appleOrangeJuice = foodProcessor(2, 4)
+// console.log(appleOrangeJuice)
