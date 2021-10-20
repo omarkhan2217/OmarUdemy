f7a35437bfbffc5241b2a547cdc58de849a24418
Omar Khan <ok.66677788@gmail.com>
2021-10-21 00:53:55 +0530

    added

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -4,6 +4,22 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+const weekdays = [`Mon`, `Tue`, `Wed`, `thu`, `fri`, `sat`, `sun`];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -11,41 +27,173 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours,
 
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = `20:00`,
-    address,
-  }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = `20:00`, address }) {
     console.log(
       `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your delecious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
 };
-// //
-// //
-// //
-// // LESSON 104 = DESTRUCTURING OBJECTS
+
+////////////////////////////////////////////
+// LESSON 111 = ENHANCED OBJECT LITERALS:-//
+////////////////////////////////////////////
+
+// //////////////////////////////////////////////////////
+// // LESSON 110 = LOOPING ARRAYS: THE FOR OF LOOP #1:-//
+// /////////////////////////////////////////////////////
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for (const item of menu) console.log(item);
+
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);
+// }
+
+// ///////////////////////////////////////////////////
+// // LESSON 108 = THE NULLISH COALESCING OPERATOR:-//
+// ///////////////////////////////////////////////////
+
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests || 10;
+// console.log(guests);
+
+// // nullish values = null and undefined
+// const guestsCorrect = restaurant.numGuests ?? 10;
+// console.log(guestsCorrect);
+
+// ////////////////////////////////////////////////
+// // LESSON 107 = SHORT CIRCUITING (&& AND ||):-//
+// ///////////////////////////////////////////////
+
+// console.log(`----------OR---------`);
+// // use ANY date type and return any data type and short circuit evaluation
+// console.log(2 || `Jonas`);
+// console.log(`` || `Jonas`);
+// console.log(true || 0);
+// console.log(undefined || null);
+
+// console.log(undefined || 0 || `` || `Hello` || 23 || null);
+
+// restaurant.numGuests = 23;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
+
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
+
+// console.log(`-------------AND-----------`);
+// console.log(0 && `jonas`);
+// console.log(7 && `Jonas`);
+
+// console.log(`Hello` && 23 && null && `jonas`);
+
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza(`Mushroo`, `SPinah`);
+// }
+
+// restaurant.orderPizza && restaurant.orderPizza(`Mushroo`, `SPin`);
+
+// /////////////////////////////////////////////
+// // LESSON 106 = REST PATTERNS AND PARRAMS:-//
+// /////////////////////////////////////////////
+
+// // 1) destructuring
+// const arr = [1, 2, ...[3, 4]];
+
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
+
+// const [pizza, , risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+// console.log(pizza, risotto, otherFood);
+
+// // objects
+// const { sat, ...weekdats } = restaurant.openingHours;
+// console.log(weekdats);
+
+// // 2) functions
+
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+//   console.log(sum);
+// };
+
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(5, 5, 2, 1, 2, 1, 5, 1);
+
+// const x = [23, 5, 7];
+// add(...x);
+
+//////////////////////////////////////////
+// // LESSON 105 = THE SPREAD OPERATOR:-//
+//////////////////////////////////////////
+
+// const arr = [7, 8, 9];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArr);
+
+// const newArr = [1, 2, ...arr];
+// console.log(newArr);
+
+// console.log(...newArr);
+
+// const newMenu = [...restaurant.mainMenu, `Gnocci`];
+// console.log(newMenu);
+
+// // Copy array
+// const mainMenuCopy = [...restaurant.mainMenu];
+
+// // join 2 arrays
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(menu);
+
+// // Iterables: arrays, strings, maps, sets, Not objects
+// const str = `jonas`;
+// const letters = [...str, ``, `S.`];
+// console.log(letters);
+// console.log(...str);
+// // console.log(`${...str} Schmedtmann`);
+
+// // real world exaples
+// const ingredients = [
+//   prompt(`Let's make pasta!, Ingredient 1?`),
+//   prompt(`Ingredient 2?`),
+//   prompt(`Ingredient 3?`),
+// ];
+// console.log(ingredients);
+
+// restaurant.orderPasta(...ingredients);
+
+// // objects
+
+// const newRestaurant = { foundedIn: 1998, ...restaurant, founder: `Guiseppe` };
+// console.log(newRestaurant);
+
+// const restaurantCopy = { ...restaurant };
+// restaurantCopy.name = `reisttoroa`;
+// console.log(restaurantCopy.name);
+// console.log(restaurant.name);
+
+////////////////////////////////////////////
+// // LESSON 104 = DESTRUCTURING OBJECTS:-//
+////////////////////////////////////////////
 
 // restaurant.orderDelivery({
 //   time: `22:30`,
@@ -88,13 +236,7 @@ const restaurant = {
 //   fri: { open: o, close: c },
 // } = openingHours;
 // console.log(o, c);
-// //
-// //
-// //
 
-// //
-// //
-// //
 // // LESSON 104 = DESTRUCTURING ARRAYS
 // const arr = [2, 3, 4];
 // const a = arr[0];
@@ -133,6 +275,3 @@ const restaurant = {
 // // Default values
 // const [p = 1, q = 1, r = 1] = [8, 9]; // anythign that doesnt have a value set changes to "1"
 // console.log(p, q, r);
-//
-//
-//
